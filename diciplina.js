62edc2534276f3fda52250678e67eb3ba07a8687
abner Davi <126670749+abnerDavi123@users.noreply.github.com>
2024-11-28 09:38:25 -0300

    Update diciplina.js

--- diciplina.js
+++ diciplina.js
@@ -1,6 +1,6 @@
 
 async function quantidadeVoto() {
-    const url = 'https://raw.githubusercontent.com//https://github.com/abnerDavi123/trabalho_grafico_json/edit/main/diciplina.jsrefs/heads/main/disciplina.json'
+    const url = 'https://raw.githubusercontent.com/Akiovictor/trabalho_json_grafico/refs/heads/main/disciplina.json'
     const res = await fetch(url)
     const dados = await res.json()
     const disciplina = Object.keys(dados)
